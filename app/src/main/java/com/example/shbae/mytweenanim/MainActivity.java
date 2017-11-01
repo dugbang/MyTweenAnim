a59d95b6afcece631635dbb26f1fa02e3896c7fa
dugbang <dugbang@gmail.com>
2017-11-01 16:11:38 +0900

    Initial commit

---
+++ app/src/main/java/com/example/shbae/mytweenanim/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.shbae.mytweenanim;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation scale = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale);
+                view.startAnimation(scale);
+            }
+        });
+    }
+}
